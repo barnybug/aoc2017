0659f7572c4e36fdea3415f82f8f3a0a7a82aefb
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-02 20:26:25 +0000

    Helpers

--- src/main/aoc2017/ListExtensions.kt
+++ src/main/aoc2017/ListExtensions.kt
@@ -1,65 +1,59 @@
 package aoc2017
 
-import kotlin.coroutines.experimental.buildSequence
-
-fun <T> List<T>.combination(k: Int) = buildSequence {
-    val list = this@combination
-
-    if (k > list.size)
-        return@buildSequence
-
-    // first index sequence: 0, 1, 2, ...
+/**
+ * Generate the combinations of a List.
+ */
+fun <T> List<T>.combination(k: Int): Sequence<List<T>> {
     val ix = (0 until k).toList().toIntArray()
-    yield(subset(list, ix))
-
-    while (true) {
+    ix[k-1]-- // first run through increments once
+    return generateSequence {
         var i = k - 1
         // find position of item that can be incremented
-        while (i >= 0 && ix[i] == list.size - k + i) {
+        while (i >= 0 && ix[i] == this.size - k + i) {
             i--
         }
-        if (i < 0) {
-            break
-        }
+        if (i < 0) return@generateSequence null
         ix[i]++ // increment this item
         ++i
         while (i < k) { // fill up remaining items
             ix[i] = ix[i - 1] + 1
             i++
         }
-        yield(subset(list, ix))
+        return@generateSequence subset(this, ix)
     }
 }
 
 // generate subset by index sequence
 fun <T> subset(input: List<T>, indices: IntArray): List<T> = indices.map { input[it] }
 
-fun <T> List<T>.permute(k: Int) = buildSequence {
-    val list = this@permute
-
-    if (k > list.size)
-        return@buildSequence
-
-    // first index sequence: 0, 1, 2, ...
-    val ix = (0 until k).toList().toIntArray()
-    yield(subset(list, ix))
-
-    while (true) {
-        var i = k - 1
-        // find position of item that can be incremented
-        while (i >= 0 && ix[i] == list.size - k + i) {
-            i--
+/**
+ * Generate the permutations of a List.
+ */
+fun <T> List<T>.permute(k: Int): Sequence<List<T>> {
+    val indices = (0 until size).toList().toIntArray()
+    val cycles = (size downTo (size-k+1)).toList().toIntArray()
+    var first = true
+    var i = k - 1
+    return generateSequence {
+        if (first) {
+            first = false
+            return@generateSequence subset(this, indices.sliceArray(0 until k))
         }
-        if (i < 0) {
-            break
-        }
-        ix[i]++ // increment this item
-        ++i
-        while (i < k) { // fill up remaining items
-            ix[i] = ix[i - 1] + 1
-            i++
-        }
-        yield(subset(list, ix))
+        while(i >= 0) {
+                cycles[i]--
+                if (cycles[i] == 0) {
+                    val tmp = indices[i]
+                    System.arraycopy(indices, i+1, indices, i, size-i-1)
+                    indices[size-1] = tmp
+                    cycles[i] = size - i
+                } else {
+                    val j = cycles[i]
+                    indices[i] = indices[size-j].also { indices[size-j] = indices[i] }
+                    i = k - 1
+                    return@generateSequence subset(this, indices.sliceArray(0 until k))
+                }
+                i -= 1
+            }
+        return@generateSequence null
     }
 }
-
--- src/test/aoc2017/ListExtensionsTests.kt
+++ src/test/aoc2017/ListExtensionsTests.kt
@@ -2,11 +2,55 @@ package aoc2017
 
 import org.junit.Test
 import org.junit.Assert.assertEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class ListExtensionsTests {
     @Test
     fun `combination`() {
         val expected = listOf(listOf(1, 2), listOf(1, 3), listOf(2, 3))
         assertEquals(expected, listOf(1, 2, 3).combination(2).toList())
+    }
+
+    private fun assertPermutation(list: List<List<Int>>, k: Int, set: Set<Int>) {
+        assertEquals(list.distinct(), list, "all elements are unique")
+        for (element in list) {
+            // correct number of elements
+            // draws from the set
+            // does not use replacement
+            assertEquals(k, set.intersect(element).size)
+        }
+    }
+
+    @Test
+    fun `permute 2`() {
+        val bag = listOf(1, 2)
+        val actual = bag.permute(2).toList()
+        assertEquals(2, actual.size)
+        assertPermutation(actual, 2, bag.toSet())
+    }
+
+    @Test
+    fun `permute 3`() {
+        val bag = listOf(1, 2, 3)
+        val actual = bag.permute(3).toList()
+        assertEquals(6, actual.size)
+        assertPermutation(actual, 3, bag.toSet())
+    }
+
+    @Test
+    fun `permute 2 of 3`() {
+        val bag = listOf(1, 2, 3)
+        val actual = bag.permute(2).toList()
+        assertEquals(6, actual.size)
+        assertPermutation(actual, 2, bag.toSet())
+    }
+
+    @Test
+    fun `permute 2 of 4`() {
+        val bag = listOf(1, 2, 3, 4)
+        val actual = bag.permute(2).toList()
+        assertEquals(12, actual.size)
+        assertPermutation(actual, 2, bag.toSet())
     }
 }
