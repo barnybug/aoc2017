6c2d5cdd6c1f10c2ea4b14e03a74acc0b898c8dc
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-06 21:54:20 +0000

    Day 5

--- src/main/aoc2017/Day05.kt
+++ src/main/aoc2017/Day05.kt
@@ -1,6 +1,32 @@
 package aoc2017
 
 class Day05(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    fun answer1(): Int {
+        val state = input.lines().map { it.toInt() }.toMutableList()
+        var pos = 0
+        var steps = 0
+        while(pos >=0 && pos < state.size) {
+            state[pos] += 1
+            pos += state[pos]-1
+            steps += 1
+        }
+        return steps
+    }
+
+    fun answer2(): Int {
+        val state = input.lines().map { it.toInt() }.toMutableList()
+        var pos = 0
+        var steps = 0
+        while(pos >=0 && pos < state.size) {
+            if (state[pos] >= 3) {
+                state[pos] -= 1
+                pos += state[pos] + 1
+            } else {
+                state[pos] += 1
+                pos += state[pos]-1
+            }
+            steps += 1
+        }
+        return steps
+    }
 }
--- src/test/aoc2017/Day05Test.kt
+++ src/test/aoc2017/Day05Test.kt
@@ -6,21 +6,21 @@ import org.junit.Test
 internal class Day05Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day05("").answer1())
+        Assert.assertEquals(5, Day05("0\n3\n0\n1\n-3").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day05(Inputs.readText(5)).answer1())
+        Assert.assertEquals(358131, Day05(Inputs.readText(5)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day05("").answer2())
+        Assert.assertEquals(10, Day05("0\n3\n0\n1\n-3").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day05(Inputs.readText(5)).answer2())
+        Assert.assertEquals(25558839, Day05(Inputs.readText(5)).answer2())
     }
 }
