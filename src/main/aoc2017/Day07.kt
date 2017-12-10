89e0c486f4bacea9033545ae49bd848d5ff4eff1
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-10 22:38:18 +0000

    Day 7

--- src/main/aoc2017/Day07.kt
+++ src/main/aoc2017/Day07.kt
@@ -1,6 +1,48 @@
 package aoc2017
 
+data class Rule(val left: String, val weight: Int, val children: List<String>)
+
 class Day07(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    private val reRule = """(\w+) \((\d+)\)(?: -> (.+))?""".toRegex()
+    private val rules = parsed()
+
+    private fun parsed() = input.lines().map { line ->
+        val m = reRule.find(line)!!
+        val (left, weight, children) = m.destructured
+        val c = if (children.isNotEmpty()) children.split(", ") else listOf()
+        Rule(left, weight.toInt(), c)
+    }.associateBy { it.left }
+
+    fun answer1(): String {
+        val all = rules.keys
+        val right = rules.flatMap { it.value.children }.toSet()
+        return (all - right).first()
+    }
+
+    class Answer(val correct: Int): Exception()
+
+    private fun check(left: String): Int {
+        val rule = rules[left]!!
+        if (rule.children.isEmpty()) return rule.weight
+
+        val values = rule.children.map { check(it) }
+        val keys = values.groupBy { it }.filter { it.value.size == 1 }.keys
+        if (keys.size == 1) {
+            val incorrect = keys.first()
+            val i = values.indexOf(incorrect)
+            val others = values.find { it != incorrect }!!
+            val child = rules[rule.children[i]]!!
+            throw Answer(child.weight - incorrect + others)
+        }
+        return values.sum() + rule.weight
+    }
+
+    fun answer2(): Int {
+        try {
+            check(answer1())
+        } catch (a: Answer) {
+            return a.correct
+        }
+        return 0
+    }
 }
--- src/test/aoc2017/Day07Test.kt
+++ src/test/aoc2017/Day07Test.kt
@@ -6,21 +6,21 @@ import org.junit.Test
 internal class Day07Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day07("").answer1())
+        Assert.assertEquals("tknk", Day07("pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\nqoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\nugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day07(Inputs.readText(7)).answer1())
+        Assert.assertEquals("eqgvf", Day07(Inputs.readText(7)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day07("").answer2())
+        Assert.assertEquals(60, Day07("pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\nqoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\nugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day07(Inputs.readText(7)).answer2())
+        Assert.assertEquals(757, Day07(Inputs.readText(7)).answer2())
     }
 }
