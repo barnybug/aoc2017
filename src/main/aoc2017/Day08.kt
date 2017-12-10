903e34a074f84eea701be1e559c393a5206ff99b
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-10 22:39:04 +0000

    Day 8

--- src/main/aoc2017/Day08.kt
+++ src/main/aoc2017/Day08.kt
@@ -1,6 +1,62 @@
 package aoc2017
 
 class Day08(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    data class Op(val reg: String, val op: String, val value: Int) {
+        fun check(state: State) = when(op) {
+            ">" -> state[reg] > value
+            "<" -> state[reg] < value
+            ">=" -> state[reg] >= value
+            "<=" -> state[reg] <= value
+            "==" -> state[reg] == value
+            "!=" -> state[reg] != value
+            else -> false
+        }
+
+        fun execute(state: State) {
+            when(op) {
+                "inc" -> state[reg] += value
+                "dec" -> state[reg] -= value
+            }
+        }
+
+        companion object {
+            fun create(s: String): Op {
+                val (reg, op, value) = s.split(" ")
+                return Op(reg, op, value.toInt())
+            }
+        }
+    }
+
+    data class Instruction(val action: Op, val condition: Op)
+
+    class State {
+        private val register = mutableMapOf<String, Int>()
+        operator fun get(r: String) = register.getOrDefault(r, 0)
+        operator fun set(r: String, value: Int) { register[r] = value }
+
+        fun maximumInRegister() = register.values.max() ?: 0
+    }
+
+    private fun instructions() = input.lines().map { line ->
+        val (left, right) = line.split(" if ")
+        Instruction(Op.create(left), Op.create(right))
+    }
+
+    fun answer1(): Int {
+        val state = State()
+        instructions().forEach { i ->
+            if (i.condition.check(state))
+                i.action.execute(state)
+        }
+        return state.maximumInRegister()
+    }
+
+    fun answer2(): Int {
+        val state = State()
+        return instructions().map { i ->
+            if (i.condition.check(state))
+                i.action.execute(state)
+            state.maximumInRegister()
+        }.max()!!
+    }
 }
--- src/test/aoc2017/Day08Test.kt
+++ src/test/aoc2017/Day08Test.kt
@@ -6,21 +6,21 @@ import org.junit.Test
 internal class Day08Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day08("").answer1())
+        Assert.assertEquals(1, Day08("b inc 5 if a > 1\na inc 1 if b < 5\nc dec -10 if a >= 1\nc inc -20 if c == 10").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day08(Inputs.readText(8)).answer1())
+        Assert.assertEquals(5075, Day08(Inputs.readText(8)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day08("").answer2())
+        Assert.assertEquals(10, Day08("b inc 5 if a > 1\na inc 1 if b < 5\nc dec -10 if a >= 1\nc inc -20 if c == 10").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day08(Inputs.readText(8)).answer2())
+        Assert.assertEquals(7310, Day08(Inputs.readText(8)).answer2())
     }
 }
