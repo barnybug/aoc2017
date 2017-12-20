e146f1680078cce1b6bc8b6fc6e26ea43541dda0
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-20 14:56:26 +0000

    Day 12

--- src/main/aoc2017/Day12.kt
+++ src/main/aoc2017/Day12.kt
@@ -1,6 +1,32 @@
 package aoc2017
 
 class Day12(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    val rules = input.lines().associate { line ->
+        val (left, right) = line.split(" <-> ")
+        left.toInt() to right.split(", ").map { it.toInt() }
+    }
+
+    fun reachable(i: Int): Set<Int> {
+        val full = mutableSetOf(i)
+        var last = setOf(i)
+        while(last.isNotEmpty()) {
+            val next = last.flatMap { rules[it] ?: listOf() }
+            last = next.toSet() - full
+            full.addAll(last)
+        }
+        return full
+    }
+
+    fun answer1() = reachable(0).size
+
+    fun answer2(): Int {
+        var groups = 0
+        var used = rules.keys
+        while(used.isNotEmpty()) {
+            val i = used.first()
+            used -= reachable(i)
+            groups++
+        }
+        return groups
+    }
 }
--- src/test/aoc2017/Day12Test.kt
+++ src/test/aoc2017/Day12Test.kt
@@ -6,21 +6,21 @@ import org.junit.Test
 internal class Day12Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day12("").answer1())
+        Assert.assertEquals(6, Day12("0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day12(Inputs.readText(12)).answer1())
+        Assert.assertEquals(113, Day12(Inputs.readText(12)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day12("").answer2())
+        Assert.assertEquals(2, Day12("0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day12(Inputs.readText(12)).answer2())
+        Assert.assertEquals(202, Day12(Inputs.readText(12)).answer2())
     }
 }
