941be73f7688846a269c6f96e6ac4d62553a0237
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-20 13:43:23 +0000

    Day 9

--- src/main/aoc2017/Day09.kt
+++ src/main/aoc2017/Day09.kt
@@ -1,6 +1,39 @@
 package aoc2017
 
 class Day09(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    fun answer1(): Int {
+        var level = 0
+        var score = 0
+        var garbage = false
+        val it = input.iterator()
+        for (ch in it) {
+            when {
+                ch == '!' -> it.next()
+                ch == '>' -> garbage = false
+                garbage -> {}
+                ch == '<' -> garbage = true
+                ch == '{' -> level++
+                ch == '}' -> {
+                    score += level
+                    level--
+                }
+            }
+        }
+        return score
+    }
+
+    fun answer2(): Int {
+        val it = input.iterator()
+        var garbage = false
+        var value = 0
+        for (ch in it) {
+            when {
+                ch == '!' -> it.next()
+                ch == '>' -> garbage = false
+                garbage -> value++
+                ch == '<' -> garbage = true
+            }
+        }
+        return value
+    }
 }
--- src/test/aoc2017/Day09Test.kt
+++ src/test/aoc2017/Day09Test.kt
@@ -6,21 +6,34 @@ import org.junit.Test
 internal class Day09Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day09("").answer1())
+        Assert.assertEquals(1, Day09("{}").answer1())
+        Assert.assertEquals(6, Day09("{{{}}}").answer1())
+        Assert.assertEquals(5, Day09("{{},{}}").answer1())
+        Assert.assertEquals(16, Day09("{{{},{},{{}}}}").answer1())
+        Assert.assertEquals(1, Day09("{<a>,<a>,<a>,<a>}").answer1())
+        Assert.assertEquals(9, Day09("{{<ab>},{<ab>},{<ab>},{<ab>}}").answer1())
+        Assert.assertEquals(9, Day09("{{<!!>},{<!!>},{<!!>},{<!!>}}").answer1())
+        Assert.assertEquals(3, Day09("{{<a!>},{<a!>},{<a!>},{<ab>}}").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day09(Inputs.readText(9)).answer1())
+        Assert.assertEquals(14204, Day09(Inputs.readText(9)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day09("").answer2())
+        Assert.assertEquals(0, Day09("<>").answer2())
+        Assert.assertEquals(17, Day09("<random characters>").answer2())
+        Assert.assertEquals(3, Day09("<<<<>").answer2())
+        Assert.assertEquals(2, Day09("<{!>}>").answer2())
+        Assert.assertEquals(0, Day09("<!!>").answer2())
+        Assert.assertEquals(0, Day09("<!!!>>").answer2())
+        Assert.assertEquals(10, Day09("<{o\"i!a,<{i<a>").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day09(Inputs.readText(9)).answer2())
+        Assert.assertEquals(6622, Day09(Inputs.readText(9)).answer2())
     }
 }
