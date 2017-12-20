125f63116d0255d99dd81a8e6e2ff72f0629483b
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-20 22:02:39 +0000

    Day 16

--- src/main/aoc2017/Day16.kt
+++ src/main/aoc2017/Day16.kt
@@ -1,6 +1,34 @@
 package aoc2017
 
 class Day16(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    val initial = ('a'..'p').joinToString("")
+
+    fun once(s: String) = input.split(",").fold(s) { v, p ->
+        when(p[0]) {
+        's' -> {
+            val n = p.substring(1).toInt()
+            v.substring(v.length - n) + v.substring(0, v.length - n)
+        }
+        'x' -> {
+            val ns = p.substring(1).split("/").map { it.toInt() }
+            v.toMutableList().apply { swap(ns[0], ns[1]) }.join()
+        }
+        'p' -> {
+            val a = v.indexOf(p[1])
+            val b = v.indexOf(p[3])
+            v.toMutableList().apply { swap(a, b) }.join()
+        }
+        else -> v
+        }
+    }
+
+    fun answer1() = once(initial)
+
+    fun answer2(): String {
+        var s = once(initial)
+        val seq = generateSequence(initial) { once(it) }.drop(1)
+        val repeat = seq.indexOfFirst { it == initial } + 1
+        val remain = (1000000000 % repeat)
+        return seq.take(remain).last()
+    }
 }
--- src/test/aoc2017/Day16Test.kt
+++ src/test/aoc2017/Day16Test.kt
@@ -6,21 +6,16 @@ import org.junit.Test
 internal class Day16Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day16("").answer1())
+        Assert.assertEquals("baedc", Day16("s1,x3/4,pe/b").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day16(Inputs.readText(16)).answer1())
-    }
-
-    @Test
-    fun `part 2 examples`() {
-        Assert.assertEquals(0, Day16("").answer2())
+        Assert.assertEquals("nlciboghjmfdapek", Day16(Inputs.readText(16)).answer1())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day16(Inputs.readText(16)).answer2())
+        Assert.assertEquals("nlciboghmkedpfja", Day16(Inputs.readText(16)).answer2())
     }
 }
