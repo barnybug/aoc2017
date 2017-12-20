be1f7dcc13fb707215878d66bcc5878ca74559c8
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-20 14:12:43 +0000

    Day 11

--- src/main/aoc2017/Day11.kt
+++ src/main/aoc2017/Day11.kt
@@ -1,6 +1,38 @@
 package aoc2017
 
+import kotlin.math.absoluteValue
+import kotlin.math.max
+
 class Day11(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    fun steps(p: Pos) = if (p.x >= 0 )
+        p.x.absoluteValue + max(p.y - p.x, 0)
+    else
+        p.x.absoluteValue + p.y
+
+    fun positions(): List<Pos> {
+        var p = Pos.Z
+        return input.split(",").map {
+            p += when(it) {
+                "n" ->
+                    Pos.U
+                "s" ->
+                    Pos.D
+                "ne" ->
+                    Pos.R
+                "se" ->
+                    Pos.R + Pos.D
+                "nw" ->
+                    Pos.L + Pos.U
+                "sw" ->
+                    Pos.L
+                else ->
+                    Pos.Z
+            }
+            p
+        }
+    }
+
+    fun answer1() = steps(positions().last())
+
+    fun answer2() = positions().map { steps(it) }.max()
 }
--- src/test/aoc2017/Day11Test.kt
+++ src/test/aoc2017/Day11Test.kt
@@ -6,21 +6,25 @@ import org.junit.Test
 internal class Day11Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day11("").answer1())
+        Assert.assertEquals(3, Day11("ne,ne,ne").answer1())
+        Assert.assertEquals(0, Day11("ne,ne,sw,sw").answer1())
+        Assert.assertEquals(2, Day11("ne,ne,s,s").answer1())
+        Assert.assertEquals(3, Day11("se,sw,se,sw,sw").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day11(Inputs.readText(11)).answer1())
+        Assert.assertEquals(794, Day11(Inputs.readText(11)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day11("").answer2())
+        Assert.assertEquals(3, Day11("ne,ne,ne").answer2())
+        Assert.assertEquals(2, Day11("ne,ne,sw,sw").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day11(Inputs.readText(11)).answer2())
+        Assert.assertEquals(1524, Day11(Inputs.readText(11)).answer2())
     }
 }
