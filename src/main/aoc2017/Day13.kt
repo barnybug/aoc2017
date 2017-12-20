47d515741df6fcacd47e0ef490a7eaefc4e02962
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-20 17:23:56 +0000

    Day 13

--- src/main/aoc2017/Day13.kt
+++ src/main/aoc2017/Day13.kt
@@ -1,6 +1,28 @@
 package aoc2017
 
 class Day13(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    val rules = input.lines().associate { line ->
+        val (left, right) = line.split(": ")
+        left.toInt() to right.toInt()
+    }
+    val size = rules.keys.max() ?: 0
+
+    fun severity(delay: Int) =
+        (0..size).sumBy { i ->
+            val depth = rules[i] ?: 0
+            if (depth > 0) {
+                val scanner = (i+delay) % (depth * 2 - 2)
+                if (scanner == 0)
+                    (i+delay) * depth
+                else
+                    0
+            } else {
+                0
+            }
+        }
+
+    fun answer1() = severity(0)
+
+    fun answer2() =
+        (0..Int.MAX_VALUE).first { severity(it) == 0 }
 }
--- src/test/aoc2017/Day13Test.kt
+++ src/test/aoc2017/Day13Test.kt
@@ -6,21 +6,21 @@ import org.junit.Test
 internal class Day13Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day13("").answer1())
+        Assert.assertEquals(24, Day13("0: 3\n1: 2\n4: 4\n6: 4").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day13(Inputs.readText(13)).answer1())
+        Assert.assertEquals(1840, Day13(Inputs.readText(13)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day13("").answer2())
+        Assert.assertEquals(10, Day13("0: 3\n1: 2\n4: 4\n6: 4").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day13(Inputs.readText(13)).answer2())
+        Assert.assertEquals(3850260, Day13(Inputs.readText(13)).answer2())
     }
 }
