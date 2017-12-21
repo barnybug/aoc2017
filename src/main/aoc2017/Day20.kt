5aa0553952168998d0c2b7cd929bff6820cceee1
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-21 16:28:52 +0000

    Day 20

--- src/main/aoc2017/Day20.kt
+++ src/main/aoc2017/Day20.kt
@@ -1,6 +1,44 @@
 package aoc2017
 
+import kotlin.math.absoluteValue
+
+typealias Pos3D = Triple<Int, Int, Int>
+
+data class Particle(val p: Pos3D, val v: Pos3D, val a: Pos3D) {
+    fun tick() = Particle(p+v+a, v+a, a)
+}
+
+operator fun Pos3D.plus(b: Pos3D) = Pos3D(first+b.first, second+b.second, third+b.third)
+
 class Day20(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    fun answer1() = input.lines().mapNotNull { line ->
+        """a=<([-0-9]+),([-0-9]+),([-0-9]+)>""".toRegex().find(line)
+    }.map { match ->
+        manhattan(parse(match))
+    }.withIndex().reversed().minBy { it.value }?.index
+
+    private fun manhattan(p: Pos3D) =
+        p.first.absoluteValue + p.second.absoluteValue + p.third.absoluteValue
+
+    private fun parse(match: MatchResult): Pos3D {
+        val (x, y, z) = match.groupValues.drop(1).map { it.toInt() }
+        return Pos3D(x, y, z)
+    }
+
+    fun answer2(): Int {
+        val particles = input.lines().map { line ->
+            val (mp, mv, ma) = """<([-0-9]+),([-0-9]+),([-0-9]+)>""".toRegex().findAll(line).toList()
+            val p = parse(mp)
+            val v = parse(mv)
+            val a = parse(ma)
+            Particle(p, v, a)
+        }
+
+        val end = (0..100).fold(particles) { state, _ ->
+            val next = state.map { it.tick() }
+            val alive = next.groupBy { it.p }.filterValues { it.size == 1 }.values.flatten()
+            alive
+        }
+        return end.size
+    }
 }
--- src/test/aoc2017/Day20Test.kt
+++ src/test/aoc2017/Day20Test.kt
@@ -6,21 +6,27 @@ import org.junit.Test
 internal class Day20Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day20("").answer1())
+        Assert.assertEquals(0, Day20("p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>\n" +
+                "p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>").answer1())
+        Assert.assertEquals(1, Day20("p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>\n" +
+                "p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day20(Inputs.readText(20)).answer1())
+        Assert.assertEquals(91, Day20(Inputs.readText(20)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day20("").answer2())
+        Assert.assertEquals(1, Day20("p=<-6,0,0>, v=<3,0,0>, a=<0,0,0>\n" +
+                "p=<-4,0,0>, v=<2,0,0>, a=<0,0,0>\n" +
+                "p=<-2,0,0>, v=<1,0,0>, a=<0,0,0>\n" +
+                "p=<3,0,0>, v=<-1,0,0>, a=<0,0,0>").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day20(Inputs.readText(20)).answer2())
+        Assert.assertEquals(567, Day20(Inputs.readText(20)).answer2())
     }
 }
