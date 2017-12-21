7958ceeb6b2108b28b2875b5c170991322daa36c
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-21 15:52:05 +0000

    Day 19

--- src/main/aoc2017/Day19.kt
+++ src/main/aoc2017/Day19.kt
@@ -1,6 +1,45 @@
 package aoc2017
 
 class Day19(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    private val grid = input.lines()
+            .mapIndexed { y, line -> line.mapIndexed { x, c -> Pos(x, y) to c } }
+            .flatMap { it }
+            .filter { it.second != ' ' }
+            .associate { it }
+    private val letters = grid.filterValues { ('A'..'Z').contains(it) }
+
+    fun sequence(): Sequence<Pos> {
+        val start = grid.filterKeys { it.y == 0 }.keys.first()
+        var dir = Pos.D
+        var word = ""
+
+        return generateSequence(start) { current ->
+            val next = current + dir
+            val c = grid.getOrDefault(next, ' ')
+            when(c) {
+                in 'A'..'Z' -> {
+                    word += c
+                    next
+                }
+                '+' -> {
+                    // change direction
+                    dir = if (grid.contains(next + dir.clockwise)) {
+                        dir.clockwise
+                    } else {
+                        dir.anticlockwise
+                    }
+                    next
+                }
+                ' ' -> {
+                    null
+                }
+                else -> next
+            }
+        }
+    }
+
+    fun answer1() = sequence().mapNotNull { letters[it] }.joinToString("")
+
+    fun answer2() = sequence().count()
+
 }
--- src/test/aoc2017/Day19Test.kt
+++ src/test/aoc2017/Day19Test.kt
@@ -6,21 +6,31 @@ import org.junit.Test
 internal class Day19Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day19("").answer1())
+        Assert.assertEquals("ABCDEF", Day19("     |          \n" +
+                "     |  +--+    \n" +
+                "     A  |  C    \n" +
+                " F---|----E|--+ \n" +
+                "     |  |  |  D \n" +
+                "     +B-+  +--+ \n").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day19(Inputs.readText(19)).answer1())
+        Assert.assertEquals("SXWAIBUZY", Day19(Inputs.readText(19)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day19("").answer2())
+        Assert.assertEquals(38, Day19("     |          \n" +
+                "     |  +--+    \n" +
+                "     A  |  C    \n" +
+                " F---|----E|--+ \n" +
+                "     |  |  |  D \n" +
+                "     +B-+  +--+ \n").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day19(Inputs.readText(19)).answer2())
+        Assert.assertEquals(16676, Day19(Inputs.readText(19)).answer2())
     }
 }
