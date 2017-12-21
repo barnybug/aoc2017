d8809f6d502faa0aeadb2116d14471b73eb3d727
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-21 10:07:04 +0000

    Day 18

--- src/main/aoc2017/Day18.kt
+++ src/main/aoc2017/Day18.kt
@@ -1,6 +1,84 @@
 package aoc2017
 
+data class Instruction(val t: String, val args: List<String>) {
+    val X: String get() = args[0]
+    val Y: String get() = args[1]
+}
+
+class Program(val ins: List<Instruction>, var regs: MutableMap<String, Long>, var inputs: MutableList<Long>, var outputs: MutableList<Long>, val mode: Int) {
+    var pc = 0
+    var sends = 0
+
+    private fun read(value: String): Long {
+        val i = value.toLongOrNull()
+        if (i != null)
+            return i
+        return regs.getOrDefault(value, 0)
+    }
+
+    fun execute(): Boolean {
+        val c = ins[pc]
+        when (c.t) {
+            "snd" -> {
+                sends++
+                outputs.add(read(c.X))
+            }
+            "set" ->
+                regs[c.X] = read(c.Y)
+            "add" ->
+                regs[c.X] = read(c.X) + read(c.Y)
+            "mul" ->
+                regs[c.X] = read(c.X) * read(c.Y)
+            "mod" ->
+                regs[c.X] = read(c.X) % read(c.Y)
+            "rcv" -> {
+                if (mode == 0) {
+                    if (read(c.X) != 0L)
+                        return false
+                } else {
+                    if (inputs.isEmpty())
+                        return false
+                    regs[c.X] = inputs.first()
+                    inputs.removeAt(0)
+                }
+            }
+            "jgz" -> {
+                if (read(c.X) > 0L)
+                    pc += read(c.Y).toInt()-1
+            }
+        }
+        pc++
+        return true
+    }
+}
+
 class Day18(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    val ins = input.lines().map { line ->
+        val l = line.split(" ")
+        Instruction(l[0], l.slice(1 until l.size))
+    }
+
+    fun answer1(): Long {
+        val prog = Program(ins, mutableMapOf(), mutableListOf(), mutableListOf(), 0)
+        while(prog.execute()) {
+        }
+        return prog.outputs.last()
+    }
+
+    fun answer2(): Int {
+        val p1 = Program(ins, mutableMapOf("p" to 0L), mutableListOf(), mutableListOf(), 1)
+        val p2 = Program(ins, mutableMapOf("p" to 1L), p1.outputs, p1.inputs, 1)
+
+        var deadlocked = false
+        while(!deadlocked) {
+            deadlocked = true
+            while(p1.execute()) {
+                deadlocked = false
+            }
+            while(p2.execute()) {
+                deadlocked = false
+            }
+        }
+        return p2.sends
+    }
 }
--- src/test/aoc2017/Day18Test.kt
+++ src/test/aoc2017/Day18Test.kt
@@ -6,21 +6,36 @@ import org.junit.Test
 internal class Day18Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day18("").answer1())
+        Assert.assertEquals(4, Day18("set a 1\n" +
+                "add a 2\n" +
+                "mul a a\n" +
+                "mod a 5\n" +
+                "snd a\n" +
+                "set a 0\n" +
+                "rcv a\n" +
+                "jgz a -1\n" +
+                "set a 1\n" +
+                "jgz a -2").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day18(Inputs.readText(18)).answer1())
+        Assert.assertEquals(4601, Day18(Inputs.readText(18)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day18("").answer2())
+        Assert.assertEquals(3, Day18("snd 1\n" +
+                "snd 2\n" +
+                "snd p\n" +
+                "rcv a\n" +
+                "rcv b\n" +
+                "rcv c\n" +
+                "rcv d").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day18(Inputs.readText(18)).answer2())
+        Assert.assertEquals(6858, Day18(Inputs.readText(18)).answer2())
     }
 }
