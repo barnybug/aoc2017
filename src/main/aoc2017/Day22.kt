f7141acab18cb9cadf7abdd197ea09d64f824487
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-22 17:31:18 +0000

    Day 22

--- src/main/aoc2017/Day22.kt
+++ src/main/aoc2017/Day22.kt
@@ -1,6 +1,81 @@
 package aoc2017
 
 class Day22(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+
+    fun show(grid: Set<Pos>) {
+        val minx = grid.minBy { it.x }!!.x
+        val maxx = grid.maxBy { it.x }!!.x
+        val miny = grid.minBy { it.y }!!.y
+        val maxy = grid.maxBy { it.y }!!.y
+        val s = (miny..maxy).map { y ->
+            (minx..maxx).map { x ->
+                if (grid.contains(Pos(x, y))) '#' else '.'
+            }.join()
+        }.joinToString("\n")
+        println(s)
+    }
+
+    fun answer1(bursts: Int): Int {
+        val centre = Pos( (input.lines().first().length)/2, (input.lines().size)/2)
+        val grid = input.lines()
+                .mapIndexed { y, line -> line.mapIndexed { x, c -> Pos(x-centre.x, y-centre.y) to c } }
+                .flatten()
+                .filter { it.second == '#' }
+                .map { it.first }.toMutableSet()
+
+        var infections = 0
+        var virus = Pos.Z
+        var dir = Pos.U
+        (1..bursts).forEach {
+            if (grid.contains(virus)) {
+                dir = dir.anticlockwise
+                grid.remove(virus)
+            }
+            else {
+                dir = dir.clockwise
+                grid.add(virus)
+                infections++
+            }
+            virus += dir
+        }
+
+        return infections
+    }
+
+    fun answer2(bursts: Int): Int {
+        val centre = Pos( (input.lines().first().length)/2, (input.lines().size)/2)
+        val grid = input.lines()
+                .mapIndexed { y, line -> line.mapIndexed { x, c -> Pos(x-centre.x, y-centre.y) to c } }
+                .flatten()
+                .filter { it.second == '#' }
+                .associate { it }
+                .toMutableMap()
+
+        var infections = 0
+        var virus = Pos.Z
+        var dir = Pos.U
+        (1..bursts).forEach {
+            when(grid[virus]) {
+                'W' -> {
+                    grid[virus] = '#'
+                    infections++
+                }
+                '#' -> {
+                    grid[virus] = 'F'
+                    dir = dir.anticlockwise
+                }
+                'F' -> {
+                    grid.remove(virus)
+                    dir = Pos.Z-dir
+                }
+                else -> {
+                    grid[virus] = 'W'
+                    dir = dir.clockwise
+                }
+            }
+            virus += dir
+        }
+
+        return infections
+    }
 }
--- src/test/aoc2017/Day22Test.kt
+++ src/test/aoc2017/Day22Test.kt
@@ -6,21 +6,34 @@ import org.junit.Test
 internal class Day22Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day22("").answer1())
+        Assert.assertEquals(5, Day22("..#\n" +
+                "#..\n" +
+                "...").answer1(7))
+        Assert.assertEquals(41, Day22("..#\n" +
+                "#..\n" +
+                "...").answer1(70))
+        Assert.assertEquals(5587, Day22("..#\n" +
+                "#..\n" +
+                "...").answer1(10000))
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day22(Inputs.readText(22)).answer1())
+        Assert.assertEquals(5348, Day22(Inputs.readText(22)).answer1(10000))
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day22("").answer2())
+        Assert.assertEquals(26, Day22("..#\n" +
+                "#..\n" +
+                "...").answer2(100))
+        Assert.assertEquals(2511944, Day22("..#\n" +
+                "#..\n" +
+                "...").answer2(10000000))
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day22(Inputs.readText(22)).answer2())
+        Assert.assertEquals(2512225, Day22(Inputs.readText(22)).answer2(10000000))
     }
 }
