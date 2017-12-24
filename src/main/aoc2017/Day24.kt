0c2043d50946d1475a4d0497e9997d8e3a8de3be
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-24 09:44:43 +0000

    Day 24

--- src/main/aoc2017/Day24.kt
+++ src/main/aoc2017/Day24.kt
@@ -1,6 +1,48 @@
 package aoc2017
 
 class Day24(private val input: String) {
-    fun answer1() = input
-    fun answer2() = input
+    private val parts = parseInput()
+
+    private fun parseInput(): MutableMap<Int, List<Int>> {
+        // parts are unique
+        val parts = mutableMapOf<Int, List<Int>>()
+        input.lines().forEach { line ->
+            val (left, right) = line.split("/").map { it.toInt() }
+            parts[left] = parts.getOrDefault(left, listOf()).plus(right)
+            if (left != right)
+                parts[right] = parts.getOrDefault(right, listOf()).plus(left)
+        }
+        return parts
+    }
+
+    private fun bridge(left: Int, score: Int, length: Int, answers: MutableList<Pair<Int, Int>>) {
+        val li = parts[left]
+        if (li == null || li.isEmpty()) {
+            answers.add(score to length)
+            return
+        }
+        return li.forEach { right ->
+//            println("$left/$right $score")
+            parts[left] = li.minusElement(right)
+            val ri = parts[right]!!
+            if (left != right)
+                parts[right] = ri.minusElement(left)
+            bridge(right, score + left + right, length+1, answers)
+            parts[left] = li
+            if (left != right)
+                parts[right] = ri
+        }
+    }
+    fun answer1(): Int {
+        val answers = mutableListOf<Pair<Int,Int>>()
+        bridge(0, 0, 0, answers)
+        return answers.map { it.first }.max()!!
+    }
+
+    fun answer2(): Int {
+        val answers = mutableListOf<Pair<Int,Int>>()
+        bridge(0, 0, 0, answers)
+        val longest = answers.map { it.second }.max()!!
+        return answers.filter { it.second == longest }.map { it.first }.max()!!
+    }
 }
--- src/test/aoc2017/Day24Test.kt
+++ src/test/aoc2017/Day24Test.kt
@@ -6,21 +6,35 @@ import org.junit.Test
 internal class Day24Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day24("").answer1())
+        Assert.assertEquals(31, Day24("0/2\n" +
+                "2/2\n" +
+                "2/3\n" +
+                "3/4\n" +
+                "3/5\n" +
+                "0/1\n" +
+                "10/1\n" +
+                "9/10").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day24(Inputs.readText(24)).answer1())
+        Assert.assertEquals(2006, Day24(Inputs.readText(24)).answer1())
     }
 
     @Test
     fun `part 2 examples`() {
-        Assert.assertEquals(0, Day24("").answer2())
+        Assert.assertEquals(19, Day24("0/2\n" +
+                "2/2\n" +
+                "2/3\n" +
+                "3/4\n" +
+                "3/5\n" +
+                "0/1\n" +
+                "10/1\n" +
+                "9/10").answer2())
     }
 
     @Test
     fun `part 2`() {
-        Assert.assertEquals(0, Day24(Inputs.readText(24)).answer2())
+        Assert.assertEquals(1994, Day24(Inputs.readText(24)).answer2())
     }
 }
