a3b6cccc8f3f2edccd4c6347a3228ef95eb5e0a5
Barnaby Gray <barnaby@pickle.me.uk>
2017-12-25 22:26:40 +0000

    Day 25

--- src/main/aoc2017/Day25.kt
+++ src/main/aoc2017/Day25.kt
@@ -1,6 +1,39 @@
 package aoc2017
 
 class Day25(private val input: String) {
-    fun answer1() = input
+    fun answer1(): Int {
+        var state = "Begin in state (.)".toRegex().find(input)!!.groupValues[1][0] - 'A'
+        val steps = "after (\\d+) steps".toRegex().find(input)!!.groupValues[1].toInt()
+        val matches = "In state (.).+?If the current value is 0:.+?Write the value ([01]).+?Move one slot to the (right|left).+?Continue with state (.).+?If the current value is 1:.+?Write the value ([01]).+?Move one slot to the (right|left).+?Continue with state (.)".toRegex(RegexOption.DOT_MATCHES_ALL).findAll(input)
+        val states = matches.map { m ->
+            val (_, fv, fd, fs, tv, td, ts) = m.destructured
+            listOf(
+                    fv.toInt(),
+                    if (fd == "right") 1 else -1,
+                    fs[0] - 'A',
+                    tv.toInt(),
+                    if (td == "right") 1 else -1,
+                    ts[0] - 'A'
+            )
+        }.toList()
+
+        val tape = mutableSetOf<Int>()
+        var tc = 0
+        (1..steps).forEach {
+            val a = states[state]
+            if (!tape.contains(tc)) {
+                if (a[0] == 1)
+                    tape.add(tc)
+                tc += a[1]
+                state = a[2]
+            } else {
+                if (a[3] == 0)
+                    tape.remove(tc)
+                tc += a[4]
+                state = a[5]
+            }
+        }
+        return tape.size
+    }
     fun answer2() = input
 }
--- src/test/aoc2017/Day25Test.kt
+++ src/test/aoc2017/Day25Test.kt
@@ -6,12 +6,33 @@ import org.junit.Test
 internal class Day25Test {
     @Test
     fun `part 1 examples`() {
-        Assert.assertEquals(0, Day25("").answer1())
+        Assert.assertEquals(3, Day25("Begin in state A.\n" +
+                "Perform a diagnostic checksum after 6 steps.\n" +
+                "\n" +
+                "In state A:\n" +
+                "  If the current value is 0:\n" +
+                "    - Write the value 1.\n" +
+                "    - Move one slot to the right.\n" +
+                "    - Continue with state B.\n" +
+                "  If the current value is 1:\n" +
+                "    - Write the value 0.\n" +
+                "    - Move one slot to the left.\n" +
+                "    - Continue with state B.\n" +
+                "\n" +
+                "In state B:\n" +
+                "  If the current value is 0:\n" +
+                "    - Write the value 1.\n" +
+                "    - Move one slot to the left.\n" +
+                "    - Continue with state A.\n" +
+                "  If the current value is 1:\n" +
+                "    - Write the value 1.\n" +
+                "    - Move one slot to the right.\n" +
+                "    - Continue with state A.").answer1())
     }
 
     @Test
     fun `part 1`() {
-        Assert.assertEquals(0, Day25(Inputs.readText(25)).answer1())
+        Assert.assertEquals(2526, Day25(Inputs.readText(25)).answer1())
     }
 
     @Test
